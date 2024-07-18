a8e691918f2ee18591bbd1c05f3bd3c6b6bcd9fe
tlomiej <bartekstaron@gmail.com>
2024-07-18 15:28:06 +0200

    add undo/redo buttons

--- src/components/ToolbarComponent/ToolbarComponent.tsx
+++ src/components/ToolbarComponent/ToolbarComponent.tsx
@@ -1,15 +1,19 @@
 import { IconButton, Toolbar } from "@mui/material";
 import React from "react";
-import LayerBasemapIcon from "calcite-ui-icons-react/LayerBasemapIcon";
+import UndoIcon from "calcite-ui-icons-react/UndoIcon";
+import RedoIcon from "calcite-ui-icons-react/RedoIcon";
 import ApplicationsIcon from "calcite-ui-icons-react/ApplicationsIcon";
 import Extent from "@arcgis/core/geometry/Extent";
 import { config } from "../../config";
+import useMapUndoRedo from "../hooks/useMapUndoRedo";
 
 interface ToolbarComponentProps {
   view: any;
 }
 
 export const ToolbarComponent: React.FC<ToolbarComponentProps> = ({ view }) => {
+  const { undoView, redoView, canUndo, canRedo } = useMapUndoRedo(view);
+
   return (
     <>
       <Toolbar>
@@ -18,7 +22,6 @@ export const ToolbarComponent: React.FC<ToolbarComponentProps> = ({ view }) => {
           edge="start"
           color="inherit"
           aria-label="open drawer"
-          sx={{ mr: 2 }}
           title="Start extent"
           onClick={() => {
             const extent = new Extent(config.EXTEND);
@@ -27,23 +30,26 @@ export const ToolbarComponent: React.FC<ToolbarComponentProps> = ({ view }) => {
         >
           <ApplicationsIcon />
         </IconButton>
-       {/*  <IconButton
+        <IconButton
           size="small"
           edge="start"
           color="inherit"
           aria-label="open drawer"
+          onClick={undoView}
+          disabled={!canUndo}
         >
-          <ApplicationsIcon />
+          <UndoIcon />
         </IconButton>
         <IconButton
           size="small"
           edge="end"
           color="inherit"
           aria-label="open drawer"
-          sx={{ mr: 2 }}
+          onClick={redoView}
+          disabled={!canRedo}
         >
-          <LayerBasemapIcon />
-        </IconButton> */}
+          <RedoIcon />
+        </IconButton>
       </Toolbar>
     </>
   );
---
+++ src/components/hooks/useMapUndoRedo.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from "react";
+import MapView from "@arcgis/core/views/MapView";
+import { Extent } from "@arcgis/core/geometry";
+
+const useMapUndoRedo = (view: MapView) => {
+  const historyRef = useRef<Extent[]>([]);
+  const historyIndexRef = useRef<number>(-1);
+  const [canUndo, setCanUndo] = useState(false);
+  const [canRedo, setCanRedo] = useState(false);
+  const isMove = useRef<boolean>(false);
+
+  useEffect(() => {
+    const handleExtentChange = () => {
+      if (view && view.stationary) {
+        if (isMove.current) {
+          isMove.current = false;
+          return;
+        }
+        const currentExtent = view.extent.clone();
+        historyRef.current.push(currentExtent);
+        historyIndexRef.current++;
+
+        setCanUndo(historyIndexRef.current > 0);
+        setCanRedo(historyIndexRef.current < historyRef.current.length - 1);
+      }
+    };
+
+    if (view) {
+      const extentChangeHandle = view.watch("stationary", handleExtentChange);
+      return () => {
+        extentChangeHandle.remove();
+      };
+    }
+  }, [view]);
+
+  const undoView = () => {
+    if (canUndo && view) {
+      isMove.current = true;
+      historyIndexRef.current--;
+      const previousExtent = historyRef.current[historyIndexRef.current];
+      view.goTo(previousExtent);
+      setCanUndo(historyIndexRef.current > 0);
+      setCanRedo(true);
+    }
+  };
+
+  const redoView = () => {
+    if (canRedo && view) {
+      isMove.current = true;
+      historyIndexRef.current++;
+      const nextExtent = historyRef.current[historyIndexRef.current];
+      view.goTo(nextExtent);
+      setCanUndo(true);
+      setCanRedo(historyIndexRef.current < historyRef.current.length - 1);
+    }
+  };
+
+  return {
+    undoView,
+    redoView,
+    canUndo,
+    canRedo,
+  };
+};
+
+export default useMapUndoRedo;
