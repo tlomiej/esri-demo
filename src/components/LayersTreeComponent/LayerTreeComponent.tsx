1b2ceeb4de759bba33ae9c8252f56e7c4afcdb42
tlomiej <bartekstaron@gmail.com>
2024-07-27 08:09:30 +0200

    init layer tree component

---
+++ src/components/LayersTreeComponent/LayerTreeComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface LayerTreeComponentProps {}
+const LayerTreeComponent: React.FC<LayerTreeComponentProps> = () => {
+  return <div>Layer tree component init</div>;
+};
+
+export default LayerTreeComponent;
