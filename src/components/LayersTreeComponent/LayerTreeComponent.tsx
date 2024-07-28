7f3831728362baa71939f0ee86b12cc212f747c4
tlomiej <bartekstaron@gmail.com>
2024-07-28 16:30:13 +0200

    display layers

--- src/components/LayersTreeComponent/LayerTreeComponent.tsx
+++ src/components/LayersTreeComponent/LayerTreeComponent.tsx
@@ -1,11 +1,19 @@
 import MapView from "@arcgis/core/views/MapView";
 import React from "react";
+import { config } from "../../config";
 
 interface LayerTreeComponentProps {
   view: MapView;
 }
 const LayerTreeComponent: React.FC<LayerTreeComponentProps> = ({ view }) => {
-  return <div>Layer tree component init</div>;
+  return (
+    <div>
+      {config.LAYERS.map((layer) => {
+        return <div>{layer.title}</div>;
+      })}
+      Layer tree component init
+    </div>
+  );
 };
 
 export default LayerTreeComponent;
